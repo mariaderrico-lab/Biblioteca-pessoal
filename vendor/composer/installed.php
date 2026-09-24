b8737a611356e0c929de19fd0c9701e6cf6581a1
mariaderrico-lab <maria.d'errico@ba.estudante.senai.br>
2026-09-24 09:28:03 -0300

    Vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mybibli/mybibli',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f3bc47419f0eda80d2b8f71a53b43912185d9f29',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mybibli/mybibli' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f3bc47419f0eda80d2b8f71a53b43912185d9f29',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
